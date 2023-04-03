40912448c13206e54c27ff3cc088d43b6585da2a
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-03 16:11:41 +0800

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,20 @@
+library(seqLogo) 
+
+df <-read.csv("m.csv")
+df
+
+p <- makePWM(df)
+slotNames(p)
+pwm(p)
+ic(p)
+consensus(p)
+seqLogo(p)
+seqLogo(p, ic.scale=FALSE)
+
+
+seqLogo(p, fill=c(A="#4daf4a", C="#377eb8", G="#ffd92f", T="#e41a1c"),
+        ic.scale=FALSE)
+
+
+r <- makePWM(df, alphabet="RNA")
+seqLogo(r, ic.scale=FALSE)
